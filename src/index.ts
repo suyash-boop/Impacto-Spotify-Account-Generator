1bd5c4c1b1c64fc61db9fce1864cb1be41b9ac91
suyash--boop <suyashpadole680@gmail.com>
2025-06-17 15:26:09 +0530

    clean push

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-import { Zyphra } from "./cli/core/init";
-Zyphra.xyz()
+// import { Zyphra } from "./cli/core/init";
+// Zyphra.xyz()
 
 
+import Mailjs from "@cemalgnlts/mailjs";
+
+export async function getOTP(){
+
+    const mailjs = new Mailjs();
+    console.log(chalkblueBright("Starting email verification"));
+
+    try {
+        await mailjs.login("992cf@punkproof.com", "6vbycnup")
+        console.log(chalk.green("Successfully logged into the mail!"))
+
+        const messages = await mailjs.getMessages();
+        console.log("Message: ", Messages);
+
+    } catch (error) {
+        console.log("error capturing otp")
+    }   
+}
+
+getOTP();
--- src/services/getOTP.ts
+++ src/services/getOTP.ts
@@ -1,18 +1,18 @@
 import Mailjs from "@cemalgnlts/mailjs";
 
-export async function emailVerify(email: string, username: string){
+export async function getOTP(email: string, username: string){
 
     const mailjs = new Mailjs();
     console.log(chalkblueBright("Starting email verification"));
 
     try {
-        await mailjs.login("0jop9@punkproof.com", "q1xzlbri")
+        await mailjs.login("992cf@punkproof.com", "6vbycnup")
         console.log(chalk.green("Successfully logged into the mail!"))
 
         const messages = await mailjs.getMessages();
         console.log("Message: ", Messages);
 
     } catch (error) {
-        
+        console.log("error capturing otp")
     }   
 }
